8159f09fc6b22e8f5e3e267d95cc06291ac6e347
Maciej Delmanowski <drybjed@drybjed.net>
2023-07-31 23:29:07 +0200

    Setup basic FIFO interface

--- src/chimed/__main__.py
+++ src/chimed/__main__.py
@@ -1,12 +1,27 @@
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2022 Maciej Delmanowski <drybjed@gmail.com>
+# Copyright (C) 2022-2023 Maciej Delmanowski <drybjed@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
-def main():
-    print("Hello, chimed")
+import atexit
+import shutil
+import sys
+import xdg
+import os
 
+
+comm_fifo = os.path.join(xdg.xdg_runtime_dir(), 'chimed', 'fifo')
+
+
+def cleanup():
+    shutil.rmtree(os.path.dirname(comm_fifo))
+
+
+def main():
+
+    os.mkdir(os.path.dirname(comm_fifo))
+    os.mkfifo(comm_fifo)
+
+atexit.register(cleanup)
 
 if __name__ == '__main__':
     try:
